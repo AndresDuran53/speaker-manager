67f75b00fe594e269e94d19773e3f5b8950e518f
Andres Duran <andresduranrod@gmail.com>
2023-10-12 00:00:56 -0600

    adding convertion for floats also

--- speakerManager/controllers/volume_controller.py
+++ speakerManager/controllers/volume_controller.py
@@ -10,7 +10,8 @@ class VolumeController:
     @staticmethod
     def set_volume(volume):
         try:
-            volume_int = int(volume)
+            volume_float = float(volume)
+            volume_int = round(volume_float)
             if 0 <= volume_int <= 100:
                 VolumeController._execute_command(volume_int)
         except:
