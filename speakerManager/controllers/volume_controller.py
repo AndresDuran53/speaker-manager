ab070506e83af92b121772415d756b037b306e7b
Andres Duran <andresduranrod@gmail.com>
2023-10-11 23:45:11 -0600

    adding volume control

---
+++ speakerManager/controllers/volume_controller.py
@@ -0,0 +1,20 @@
+import subprocess
+
+class VolumeController:
+
+    @staticmethod
+    def _execute_command(volume):
+        command = f'amixer set Master -M {volume}%'
+        subprocess.run(command, shell=True)
+
+    @staticmethod
+    def set_volume(volume):
+        try:
+            volume_int = int(volume)
+            if 0 <= volume_int <= 100:
+                VolumeController._execute_command(volume_int)
+        except:
+            pass
+            
+            
+            
