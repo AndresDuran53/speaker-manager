0ca5bfd22faaf17088bcb188335631eb54e742b0
Andres Duran <andresduranrod@gmail.com>
2023-10-11 20:21:42 -0600

    small fix

--- speakerManager/services/raspotify_service.py
+++ speakerManager/services/raspotify_service.py
@@ -2,8 +2,9 @@ class RaspotifyService:
     _status: bool
     _is_active: bool
 
-    def __init__(self, status = False) -> None:
+    def __init__(self, status = False, is_active = False) -> None:
         self._status = status
+        self._is_active = is_active
 
     def update_status(self,message_recieved) -> bool:
         new_active_state = None
@@ -12,7 +13,7 @@ class RaspotifyService:
             new_active_state = False
         elif(message_recieved == "playing" or message_recieved == "paused" or message_recieved == "changed"):
             new_active_state = True
-            
+
         if(new_active_state is not None and new_active_state != self._is_active):
             self._is_active = new_active_state
             return True
