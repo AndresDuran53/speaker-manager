c654fd25a1ab7c52b30934a7cef3a20aa83d1c8e
Andres Duran <andresduranrod@gmail.com>
2024-03-29 17:57:25 -0600

    test modification

--- speakerManager/test.py
+++ speakerManager/test.py
@@ -1,5 +1,6 @@
 from utils.ConfigurationReader import ConfigurationReader
 from services.spotify_service import SpotifyService, SpotifyConfig
+import time
 
 config_data = ConfigurationReader().read_config_file()
 config = SpotifyConfig.from_json(config_data)
@@ -10,3 +11,9 @@ print(f"devices {devices}")
 for device in devices:
     print(device.name)
     print(device.id)
+
+print(spotify_service.is_playing())
+print(spotify_service.is_raspotify_playing())
+spotify_service.pause_song_if_necessary()
+time.sleep(2)
+#spotify_service.play_song()
