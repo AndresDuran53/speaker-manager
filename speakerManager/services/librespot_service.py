936852d9992943284484a3430a448e6c50f7129f
Andres Duran <andresduranrod@gmail.com>
2024-06-24 19:13:14 -0600

    Fixing activity_status_timed_out Librespot

--- speakerManager/services/librespot_service.py
+++ speakerManager/services/librespot_service.py
@@ -6,6 +6,7 @@ class LibreSpotService:
     _status: bool
     _is_active: bool
     _last_modified: datetime
+    _last_active_signal: datetime
 
     def __init__(self, status = "stop", is_active = False, logger=CustomLogging("logs/librespot.log")) -> None:
         self.logger = logger
@@ -13,6 +14,7 @@ class LibreSpotService:
         self._status = status
         self._is_active = is_active
         self._last_modified = datetime.now()
+        self._last_active_signal = datetime.now()
 
     def update_status(self, message_recieved: str) -> bool:
         new_active_state = None
@@ -21,6 +23,9 @@ class LibreSpotService:
             new_active_state = False
         elif(message_recieved == "start" or message_recieved == "play" or message_recieved == "pause" or message_recieved == "change"):
             new_active_state = True
+
+        if(message_recieved == "start" or message_recieved == "play"):
+            self._last_active_signal = datetime.now()
 
         if(new_active_state is not None and new_active_state != self._is_active):
             self._is_active = new_active_state
@@ -44,12 +49,12 @@ class LibreSpotService:
     def activity_status_timed_out(self) -> bool:
         if(self._is_active and self._status != "start" and self._status != "play"):
             current_time = datetime.now()
-            time_difference = current_time - self._last_modified
+            time_difference = current_time - self._last_active_signal
             if time_difference.total_seconds() >= 3600:
+                self.logger.info(f"LibreSpot stopping due timed out | Last_active_signal: {self._last_active_signal}")
                 self.execute_time_out()
                 return True
         return False
     
     def execute_time_out(self):
-        self._status = "stop"
-        self._is_active = False
+        self.update_status("stop")
