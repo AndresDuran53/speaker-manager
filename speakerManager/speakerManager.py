fc34abfe80675bebeb979f42a0a0fa97e3b240c1
Andres Duran <andresduranrod@gmail.com>
2026-05-19 01:50:23 -0600

    Renaming functions and adding test_action script

--- speakerManager/speakerManager.py
+++ speakerManager/speakerManager.py
@@ -26,6 +26,14 @@ class SpeakerManager():
     api_config_file = "data/text-to-speech-api.json"
     configuration_completed = False
 
+    CMD_SPOTIFY_EVENT = "Spotify Event"
+    CMD_REPRODUCE_SOUND = "Reproduce Sound"
+    CMD_STOP_SOUND = "Stop Sound"
+    CMD_REPRODUCE_TTS = "Reproduce Tts"
+    CMD_REPRODUCE_TTS_ES = "Reproduce Tts-Es"
+    CMD_SET_VOLUME = "Set Volume"
+    ASSISTANT_RECOGNITION_AUDIO_ID = "assistantRecognition"
+
     def __init__(self):
         self.logger = CustomLogging(self.loggin_path)
         self.logger.info("Creating Speaker Manager...")
@@ -39,7 +47,7 @@ class SpeakerManager():
 
         #Setting Mqtt config
         mqtt_config = MqttConfig.from_json(config_data)
-        self.mqtt_service = MqttService(mqtt_config=mqtt_config, process_message=self.read_new_message, logger=self.logger)
+        self.mqtt_service = MqttService(mqtt_config=mqtt_config, process_message=self.on_mqtt_message, logger=self.logger)
 
         #Set Audios
         self.audio_controller = AudioController(config_data, logger=self.logger)
@@ -71,7 +79,7 @@ class SpeakerManager():
         
         self.configuration_completed = True
 
-    def read_new_message(self, topic_recieved: str, message_recieved: str):
+    def on_mqtt_message(self, topic_recieved: str, message_recieved: str):
         if(not self.configuration_completed):
             return
         
@@ -84,50 +92,50 @@ class SpeakerManager():
         #If the message is a command to the program
         command_name = self.mqtt_service.get_command_from_topic(topic_recieved)
         if(command_name):
-            self.excecute_command(command_name, topic_recieved, message_recieved)
+            self.execute_command(command_name, topic_recieved, message_recieved)
             return
 
-    def excecute_command(self, command_name:str, topic_recieved:str, message:str):
-        if("Spotify Event" == command_name):
+    def execute_command(self, command_name:str, topic_recieved:str, message:str):
+        if(self.CMD_SPOTIFY_EVENT == command_name):
             librespot_changed = self.librespot.update_status(message)
             if(librespot_changed):
                 librespot_is_active = self.librespot.is_active()
                 spotify_audio_id = self.librespot.get_audio_id()
                 if(librespot_is_active):
-                    self.try_to_turn_on_speakers(spotify_audio_id,self.speaker_list)
+                    self.wake_speakers(spotify_audio_id, self.speaker_list)
                 else:
-                    self.remove_playing_file(spotify_audio_id)
+                    self.on_audio_finished(spotify_audio_id)
             return
         
         topic_recieved_split = topic_recieved.split("/")
         if(len(topic_recieved_split)<2): return
         rooms = topic_recieved_split[-2]
         
-        if("Reproduce Sound" == command_name):
+        if(self.CMD_REPRODUCE_SOUND == command_name):
             self.audio_controller.add_new_audio_request(message, rooms, stop=False)
-        elif("Stop Sound" == command_name):
+        elif(self.CMD_STOP_SOUND == command_name):
             self.audio_controller.add_new_audio_request(message, rooms, stop=True)
-        elif("Reproduce Tts" == command_name):
-            self.generate_tts_audio(message, rooms, "en")
-        elif("Reproduce Tts-Es" == command_name):
-            self.generate_tts_audio(message, rooms, "es")
-        elif("Set Volume" == command_name):
+        elif(self.CMD_REPRODUCE_TTS == command_name):
+            self.synthesize_and_queue_tts(message, rooms, "en")
+        elif(self.CMD_REPRODUCE_TTS_ES == command_name):
+            self.synthesize_and_queue_tts(message, rooms, "es")
+        elif(self.CMD_SET_VOLUME == command_name):
             VolumeController.set_volume(message)
 
-    def generate_tts_audio(self, message_recieved, rooms, language="en"):
+    def synthesize_and_queue_tts(self, message_recieved, rooms, language="en"):
         file_generated = self.textToSpeechGenerator.generate_audio_file(message_recieved, language)
         if(file_generated):
             self.audio_controller.add_new_audio_request("tts",rooms)
 
-    def check_next_message(self):
+    def process_next_queued_requests(self):
         next_to_reproduce = self.audio_controller.get_next_to_reproduce()
         if(next_to_reproduce!=None):
-            self.reproduce_message(next_to_reproduce)
+            self.handle_play_request(next_to_reproduce)
         next_to_stop = self.audio_controller.get_next_to_stop()
         if(next_to_stop!=None):
-            self.stop_message(next_to_stop)
+            self.handle_stop_request(next_to_stop)
     
-    def find_speakers(self, rooms) -> list[SpeakerDevice]:
+    def get_speakers_for_rooms(self, rooms) -> list[SpeakerDevice]:
         device_list: list[Speaker] = self.speaker_list[:] + self.chromecast_list[:]
         speakers_found = []
         roomsFound = self.room_controller.get_rooms_from_topic(rooms)
@@ -140,26 +148,31 @@ class SpeakerManager():
                     speakers_found.append(speaker_found)
         return speakers_found
 
-    def reproduce_message(self, audio_requests:AudioRequests):
+    def handle_play_request(self, audio_requests:AudioRequests):
+        # 1. Resolve speakers and audio config
         rooms = audio_requests.rooms
         audio_id = audio_requests.audioId
-        speakers = self.find_speakers(rooms)
+        speakers = self.get_speakers_for_rooms(rooms)
         audio_config = self.audio_controller.get_audio_config_by_id(audio_id)
         if(audio_config is None):
             self.logger.warning(f"No audio filename found for {audio_id}")
             return
 
-        self.try_to_turn_on_speakers(audio_id,speakers)
+        # 2. Wake speakers before playback begins
+        self.wake_speakers(audio_id, speakers)
+
+        # 3. Duck Spotify volume if librespot is streaming
         if(self.use_spotify_service):
             if(self.librespot.is_active()):
                 self.logger.info(f"LibreSpot is playing something")
                 self.spotify_service.decrease_volume_if_necessary()
             else:
                 self.logger.info(f"LibreSpot is not active")
-        #time.sleep(2)
-        self.executeAplay(speakers,audio_config)
 
-    def try_to_turn_on_speakers(self, audio_id: str, speakers_list: list[SpeakerDevice]):
+        # 4. Start audio playback
+        self.start_playback(speakers, audio_config)
+
+    def wake_speakers(self, audio_id: str, speakers_list: list[SpeakerDevice]):
         self.logger.info("Turning on Speakers...")
         pending_speakers = []
         for speaker_unknow in speakers_list:
@@ -187,19 +200,19 @@ class SpeakerManager():
             self.logger.error("[Chromecast Error]: An exception occurred playing chromecast")
         self.logger.info(f"[Chromecasts] Done.")
 
-    def stop_message(self, audio_requests:AudioRequests):
+    def handle_stop_request(self, audio_requests:AudioRequests):
         audio_id = audio_requests.audioId
         audio_config = self.audio_controller.get_audio_config_by_id(audio_id)
         if(audio_config==None): return # Close if not filename founded
-        self.killAplayProcess(audio_config)
+        self.stop_playback(audio_config)
 
-    def executeAplay(self, speakers, audio_config:AudioConfig):
+    def start_playback(self, speakers, audio_config:AudioConfig):
         self.logger.info(f"Reproducing Audio: {audio_config.file_name}")
         audio_id = audio_config.id
         try:
             if(self.audio_controller.is_audio_playing(audio_id)):
                 self.logger.info("Audio already executing")
-                self.killAplayProcess(audio_config)
+                self.stop_playback(audio_config)
             #self.reproduce_on_chromecasts(speakers,audio_config)
             sub_process_aux = self.audio_process_manager.execute_audio_process(audio_config)
             self.audio_controller.link_process_with_audio(audio_id,sub_process_aux)
@@ -207,7 +220,7 @@ class SpeakerManager():
         except:
             self.logger.error("[Aplay Error]: An exception occurred using Aplay")
 
-    def killAplayProcess(self, audioConfig:AudioConfig):
+    def stop_playback(self, audioConfig:AudioConfig):
         audio_id = audioConfig.id
         self.logger.info(f"Stopping Audio file: {audio_id}...")
         try:
@@ -216,38 +229,38 @@ class SpeakerManager():
         except:
             self.logger.error(f"Unable to kill audio file: {audio_id}")
 
-    def check_playing_files(self):
+    def update_playback_state(self):
         queue_files_playing = self.audio_controller.get_queue_files_playing()
         for audio_id, sub_process_aux in list(queue_files_playing.items()):
             if self.audio_process_manager.subprocess_ended(audio_id):
-                self.remove_playing_file(audio_id)
+                self.on_audio_finished(audio_id)
         if (self.use_spotify_service and not queue_files_playing and self.spotify_service.has_to_restore_volume()):
             self.spotify_service.restore_volume()
 
-    def remove_playing_file(self, audio_id:str):
+    def on_audio_finished(self, audio_id:str):
         self.audio_controller.remove_playing_audio(audio_id)
         removed_audio_speakers = self.audio_speaker_manager.remove_audio_from_all_speakers(audio_id)
         empty_speakers = self.audio_speaker_manager.get_empty_speakers()
         speakers_to_turn_off = [speaker_aux for speaker_aux in removed_audio_speakers if speaker_aux in empty_speakers]
         for speaker_aux in speakers_to_turn_off:
-            if(audio_id!='assistantRecognition'):
+            if(audio_id != self.ASSISTANT_RECOGNITION_AUDIO_ID):
                 self.logger.info(f"Turning off {speaker_aux.id}")
                 speaker_aux.turn_off_if_apply()
             else:
-                self.logger.info(f"Speakers will remain on due to Audio ID equals: assistantRecognition")
+                self.logger.info(f"Speakers will remain on due to Audio ID equals: {self.ASSISTANT_RECOGNITION_AUDIO_ID}")
 
-    def check_timeouts(self):
+    def check_librespot_timeout(self):
         spotify_audio_id = self.librespot.get_audio_id()
         spotify_timed_out = self.librespot.activity_status_timed_out()
         if(spotify_timed_out):
-            self.remove_playing_file(spotify_audio_id)
+            self.on_audio_finished(spotify_audio_id)
 
     def run_loop(self):
         self.logger.info("Executing reproduceThreadLoop")
         while True:
-            self.check_next_message()
-            self.check_playing_files()
-            self.check_timeouts()
+            self.process_next_queued_requests()
+            self.update_playback_state()
+            self.check_librespot_timeout()
             time.sleep(0.2)
 
     @classmethod
---
+++ speakerManager/test_actions.py
@@ -0,0 +1,217 @@
+"""
+SpeakerManager test client.
+
+Sends MQTT commands directly to the broker, exercising the same topics the
+running system subscribes to.  Run from the repository root so the config
+file is resolved correctly:
+
+    python speakerManager/test_actions.py list
+    python speakerManager/test_actions.py speaker-on 25070A
+    python speakerManager/test_actions.py play CloseGarage cocina
+    python speakerManager/test_actions.py tts "Hello world" habitacion
+    python speakerManager/test_actions.py volume 60
+"""
+
+import argparse
+import json
+import sys
+import time
+
+import paho.mqtt.client as mqtt
+
+CONFIG_FILE = "data/configuration.json"
+
+
+# ---------------------------------------------------------------------------
+# Config helpers
+# ---------------------------------------------------------------------------
+
+def load_config() -> dict:
+    try:
+        with open(CONFIG_FILE) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        sys.exit(f"Error: config file not found at '{CONFIG_FILE}'. Run this script from the repository root.")
+
+
+def find_device(config: dict, speaker_id: str) -> dict | None:
+    for device in config.get("devices", []):
+        if device["id"] == speaker_id:
+            return device
+    return None
+
+
+def build_speaker_message(device: dict, power: str) -> str:
+    """power: '1' (on) or '0' (off). Returns the formatted MQTT payload."""
+    status_value = device["status"][power]
+    return device["template"].replace("%_v%", status_value)
+
+
+# ---------------------------------------------------------------------------
+# MQTT connection
+# ---------------------------------------------------------------------------
+
+def connect_mqtt(config: dict) -> mqtt.Client:
+    mqtt_cfg = config["mqtt"]
+    client = mqtt.Client(client_id="SpeakerManagerTest", clean_session=True)
+    client.username_pw_set(mqtt_cfg["mqttUser"], mqtt_cfg["mqttPass"])
+    client.connect(mqtt_cfg["brokerAddress"])
+    client.loop_start()
+    return client
+
+
+def publish_and_disconnect(client: mqtt.Client, topic: str, message: str):
+    print(f"  → topic:   {topic}")
+    print(f"  → message: {message}")
+    client.publish(topic, message, qos=1)
+    time.sleep(0.5)  # let the QoS-1 handshake complete before disconnecting
+    client.loop_stop()
+    client.disconnect()
+    print("Done.")
+
+
+# ---------------------------------------------------------------------------
+# Actions
+# ---------------------------------------------------------------------------
+
+def action_list(config: dict):
+    print("\n=== Speakers ===")
+    for d in config.get("devices", []):
+        print(f"  {d['id']}  ({d['type']})")
+        print(f"      publish:   {d['publishTopic']}")
+        print(f"      subscribe: {d['subscribeTopic']}")
+
+    cc = config.get("chromecasts", {})
+    if cc.get("devices"):
+        print("\n=== Chromecasts ===")
+        for name in cc["devices"]:
+            print(f"  {name}")
+
+    print("\n=== Rooms ===")
+    for r in config.get("rooms", []):
+        speakers = ", ".join(r["devices"]) or "(none)"
+        print(f"  {r['name']}  →  {speakers}")
+
+    print("\n=== Audio IDs ===")
+    for a in config.get("audios", []):
+        print(f"  {a['id']:<30}  {a['file_name']}")
+
+
+def action_speaker_on(client: mqtt.Client, config: dict, speaker_id: str):
+    device = find_device(config, speaker_id)
+    if device is None:
+        sys.exit(f"Error: speaker '{speaker_id}' not found. Run 'list' to see available speakers.")
+    print(f"Turning ON speaker '{speaker_id}'")
+    publish_and_disconnect(client, device["publishTopic"], build_speaker_message(device, "1"))
+
+
+def action_speaker_off(client: mqtt.Client, config: dict, speaker_id: str):
+    device = find_device(config, speaker_id)
+    if device is None:
+        sys.exit(f"Error: speaker '{speaker_id}' not found. Run 'list' to see available speakers.")
+    print(f"Turning OFF speaker '{speaker_id}'")
+    publish_and_disconnect(client, device["publishTopic"], build_speaker_message(device, "0"))
+
+
+def action_play(client: mqtt.Client, audio_id: str, room: str):
+    print(f"Playing '{audio_id}' in room '{room}'")
+    publish_and_disconnect(client, f"speaker-message/{room}/reproduce", audio_id)
+
+
+def action_stop(client: mqtt.Client, audio_id: str, room: str):
+    print(f"Stopping '{audio_id}' in room '{room}'")
+    publish_and_disconnect(client, f"speaker-message/{room}/stop", audio_id)
+
+
+def action_tts(client: mqtt.Client, text: str, room: str, lang: str):
+    suffix = "tts" if lang == "en" else "tts-es"
+    print(f"TTS ({lang}) in room '{room}': \"{text}\"")
+    publish_and_disconnect(client, f"speaker-message/{room}/{suffix}", text)
+
+
+def action_volume(client: mqtt.Client, value: int):
+    if not 0 <= value <= 100:
+        sys.exit("Error: volume must be between 0 and 100.")
+    print(f"Setting volume to {value}")
+    publish_and_disconnect(client, "speaker-message/set-volume", str(value))
+
+
+def action_spotify(client: mqtt.Client, event: str):
+    print(f"Sending Spotify event: '{event}'")
+    publish_and_disconnect(client, "spotify/event", event)
+
+
+# ---------------------------------------------------------------------------
+# CLI
+# ---------------------------------------------------------------------------
+
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="SpeakerManager test client — sends MQTT commands to the running system.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    sub = parser.add_subparsers(dest="action", required=True)
+
+    sub.add_parser("list", help="List speakers, rooms, and audio IDs from config")
+
+    p = sub.add_parser("speaker-on", help="Turn on a speaker (sends directly to its MQTT topic)")
+    p.add_argument("speaker_id", help="Device ID, e.g. 25070A")
+
+    p = sub.add_parser("speaker-off", help="Turn off a speaker")
+    p.add_argument("speaker_id", help="Device ID, e.g. 25070A")
+
+    p = sub.add_parser("play", help="Play an audio file in a room")
+    p.add_argument("audio_id", help="Audio ID from config, e.g. CloseGarage")
+    p.add_argument("room", help="Room name, e.g. cocina")
+
+    p = sub.add_parser("stop", help="Stop a playing audio in a room")
+    p.add_argument("audio_id", help="Audio ID")
+    p.add_argument("room", help="Room name")
+
+    p = sub.add_parser("tts", help="Speak text aloud in a room (English)")
+    p.add_argument("text", help="Text to synthesize")
+    p.add_argument("room", help="Room name")
+
+    p = sub.add_parser("tts-es", help="Speak text aloud in a room (Spanish)")
+    p.add_argument("text", help="Text to synthesize")
+    p.add_argument("room", help="Room name")
+
+    p = sub.add_parser("volume", help="Set master volume (0–100)")
+    p.add_argument("value", type=int, help="Volume level")
+
+    p = sub.add_parser("spotify", help="Simulate a librespot Spotify event")
+    p.add_argument("event", choices=["start", "play", "pause", "stop", "change"])
+
+    return parser
+
+
+def main():
+    args = build_parser().parse_args()
+    config = load_config()
+
+    if args.action == "list":
+        action_list(config)
+        return
+
+    client = connect_mqtt(config)
+
+    if args.action == "speaker-on":
+        action_speaker_on(client, config, args.speaker_id)
+    elif args.action == "speaker-off":
+        action_speaker_off(client, config, args.speaker_id)
+    elif args.action == "play":
+        action_play(client, args.audio_id, args.room)
+    elif args.action == "stop":
+        action_stop(client, args.audio_id, args.room)
+    elif args.action == "tts":
+        action_tts(client, args.text, args.room, "en")
+    elif args.action == "tts-es":
+        action_tts(client, args.text, args.room, "es")
+    elif args.action == "volume":
+        action_volume(client, args.value)
+    elif args.action == "spotify":
+        action_spotify(client, args.event)
+
+
+if __name__ == "__main__":
+    main()
